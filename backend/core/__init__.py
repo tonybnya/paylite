47b26fd8e98808c0b24833a39d9e9a88a8cf050a
Tony <tonbnya@gmail.com>
2026-02-09 19:09:05 +0100

    perf: define factory pattern to handle db init, blueprints register, global errors 404 & 500

---
+++ backend/core/__init__.py
@@ -0,0 +1,53 @@
+"""
+Script Name : __init__.py
+Description : This is where the factory lives
+Author      : @tonybnya
+"""
+
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from config import config_dict
+
+db = SQLAlchemy()
+
+
+def create_app(config_name="dev"):
+    app = Flask(__name__)
+
+    # load configuration
+    app.config.from_object(config_dict[config_name])
+
+    # bind extensions to the app instance
+    db.init_app(app)
+
+    # register blueprints (the routes)
+    from .routes import core_bp
+    from users.routes import users_bp
+    from wallets.routes import wallets_bp
+    from transactions.routes import transactions_bp
+
+    app.register_blueprint(core_bp)
+    app.register_blueprint(users_bp)
+    app.register_blueprint(wallets_bp)
+    app.register_blueprint(transactions_bp)
+
+
+    # global error handler for 404
+    @app.errorhandler(404)
+    def not_found_error(error):
+        return jsonify({
+            "success": False,
+            "data": None,
+            "error": "Resource not found"
+        }), 404
+
+    # global error handler for 500
+    @app.errorhandler(500)
+    def internal_error(error):
+        return jsonify({
+            "success": False,
+            "data": None,
+            "error": "An internal server error occurred"
+        }), 500
+
+    return app
