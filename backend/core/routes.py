2812a6ff6d98778d319e1984795d1621df01a8b1
Tony <tonbnya@gmail.com>
2026-02-13 17:13:59 +0100

    chore(core): add minor changes to core endpoints responses

--- backend/core/routes.py
+++ backend/core/routes.py
@@ -12,12 +12,13 @@ core_bp = Blueprint('core', __name__)
 
 @core_bp.route('/', methods=['GET'])
 def index():
-    return "PayLite Application Factory!"
+    return "PayLite API Application Factory!"
 
 
 @core_bp.route('/health', methods=['GET'])
 def health_check():
     return {
         "status": "ok",
+        "service": "PayLite API",
         "timestamp": datetime.now()
     }, 200
